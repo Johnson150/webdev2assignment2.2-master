436c8e5a6a9d2c242937296aaf789f3428351729
isaacm31 <110354171+isaacm31@users.noreply.github.com>
2024-03-12 22:38:16 -0600

    Update StudentList.js

--- app/components/StudentList.js
+++ app/components/StudentList.js
@@ -1,15 +1,25 @@
+// Import the React library to enable the use of React features within this component.
 import React from 'react';
 
+// Define a functional component named StudentList that takes a prop 'students' which is expected to be an array of student objects.
 const StudentList = ({ students }) => {
+  // Return JSX for rendering the student list.
   return (
+    // A div wrapper for the list with text centered.
     <div className="text-center">
+      {/* Unordered list to display each student as a list item */}
       <ul>
+        {/* Iterate over the 'students' array passed as a prop using the map function. For each 'student' object, render a list item. */}
         {students.map(student => (
+          // List item with a unique key derived from the student's first and last name to help React identify which items have changed, are added, or are removed.
           <li key={`${student.firstName}-${student.lastName}`}>
+            {/* Display student's first and last name with a strong emphasis on 'First Name' label */}
             <div>
               <strong>First Name:</strong> {student.firstName} {student.lastName}
             </div>
+            {/* Display student's date of birth with a strong emphasis on 'Date of Birth' label */}
             <div><strong>Date of Birth:</strong> {student.dateOfBirth}</div>
+            {/* Display student's grade with a strong emphasis on 'Grade' label */}
             <div><strong>Grade:</strong> {student.grade}</div>
           </li>
         ))}
@@ -18,4 +28,5 @@ const StudentList = ({ students }) => {
   );
 };
 
+// Export the StudentList component so it can be used in other parts of the application.
 export default StudentList;
